f053df0c5c682a8529894c8b781afc92e929973b
Adrian Gonzalez Madruga <adriangonzalezmadruga@gmail.com>
2021-10-17 23:26:55 -0400

    Squashed commit of the following:
    commit f5a70001fde20ff8288f8046444e0191620eba1d
    Author: Adrian Gonzalez Madruga <adriangonzalezmadruga@gmail.com>
    Date:   Sun Oct 17 23:26:11 2021 -0400
    
        Created Search bar from list for future usage

--- app/src/main/java/com/jam/recipeassistant/TestFragment2.kt
+++ app/src/main/java/com/jam/recipeassistant/TestFragment2.kt
@@ -1,12 +1,11 @@
 package com.jam.recipeassistant
 
 import android.os.Bundle
+import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.jam.recipeassistant.databinding.FragmentTest1Binding
+
 import com.jam.recipeassistant.databinding.FragmentTest2Binding
 
 class TestFragment2 : Fragment() {
@@ -19,21 +18,46 @@ class TestFragment2 : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentTest2Binding.inflate(this.layoutInflater, container, false)
 
-        binding.screen3btn.setOnClickListener {
+        /*binding.screen3btn.setOnClickListener {
             viewScreen3()
         }
         binding.screen1btn.setOnClickListener {
             viewScreen1()
-        }
+        }*/
+
+        var items:Array<String> = arrayOf("Beef Strogenof","Beef Wellington","Oven Baked Salmon","Ceaser Salad","Over Easy Egg","Eggs Benedict","Chicken Karage","Kung-Pow Chicken","Chicken Noodle Soup")
+
+        val recipeAdapter : ArrayAdapter<String> = activity?.let {ArrayAdapter(it,android.R.layout.simple_list_item_1,
+            items
+        )}!!
+
+        binding.searchList.adapter = recipeAdapter;
+
+        binding.searchView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                if (items.contains(query)){
+                    recipeAdapter.filter.filter(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                recipeAdapter.filter.filter(newText)
+                return false
+            }
+
+
+        })
 
         return binding.root
     }
 
-    fun viewScreen3(){
+    /*fun viewScreen3(){
         findNavController().navigate(R.id.action_testFragment2_to_testFragment3)
     }
 
     fun viewScreen1(){
         findNavController().navigate(R.id.action_testFragment2_to_testFragment1)
-    }
+    }*/
 }
