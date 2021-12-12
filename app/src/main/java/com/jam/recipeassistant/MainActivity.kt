0d59b0a8162da26acd32c4fa02955ab18806f4ef
Adrian Gonzalez Madruga <adriangonzalezmadruga@gmail.com>
2021-12-12 15:11:16 -0500

    added google ad mob into app
    awaiting account verification however banner ad is on and ad mob is initialized

--- app/src/main/java/com/jam/recipeassistant/MainActivity.kt
+++ app/src/main/java/com/jam/recipeassistant/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottomNavView.setupWithNavController(navController)
+        MobileAds.initialize(this) {}
 
     }
 
