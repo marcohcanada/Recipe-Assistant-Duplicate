6c50b0fe72b1a1003690becd03e484d006689b5c
Adrian Gonzalez Madruga <35780225+Adrian-Gonzalez-Madruga@users.noreply.github.com>
2022-01-30 19:25:35 -0500

    push local merge conflict

--- app/src/main/java/com/jam/recipeassistant/CreateFragment.kt
+++ app/src/main/java/com/jam/recipeassistant/CreateFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.jam.recipeassistant.databinding.FragmentCreateBinding
-import com.jam.recipeassistant.model.SQLConnection
 
 class CreateFragment : Fragment() {
 
@@ -16,24 +14,10 @@ class CreateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentCreateBinding.inflate(this.layoutInflater, container, false)
-
-        binding.button.setOnClickListener {
-            viewScreen1()
-        }
-        //ask internet permission
 
         return binding.root
     }
 
-    fun viewScreen1(){
-        var conn = SQLConnection()
-        var result = conn.executeQuery("SELECT * FROM Ingredient");
-        if(result != null) {
-            result.next()
-            System.out.println(result.getString(2))
-        }
-        //findNavController().navigate(R.id.action_testFragment3_to_testFragment1)
-    }
+
 }
