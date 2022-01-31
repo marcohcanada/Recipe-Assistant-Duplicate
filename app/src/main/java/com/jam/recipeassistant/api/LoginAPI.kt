485e1d694f9920ec89f92c05afae1067be5a6c62
Marco Hidalgo-Romero <marcohcanada@gmail.com>
2022-01-30 22:29:23 -0500

    LoginAPI created

--- app/src/main/java/com/jam/recipeassistant/api/LoginAPI.kt
+++ app/src/main/java/com/jam/recipeassistant/api/LoginAPI.kt
@@ -3,6 +3,7 @@ package com.jam.recipeassistant.api
 import android.content.res.Resources
 import android.provider.Settings.Global.getString
 import com.jam.recipeassistant.R
+import com.jam.recipeassistant.model.Login.UserLogin
 import com.jam.recipeassistant.model.Suggestions.RecipeCard
 import com.jam.recipeassistant.model.Suggestions.User
 import com.squareup.moshi.Json
@@ -16,33 +17,21 @@ import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import okhttp3.MediaType.Companion.toMediaType
 
-class SuggestionsAPI {
-
+/**
+ * Created by 991470628 : MARCO HIDALGO ROMERO
+ * on 2022-01-30
+ */
+class LoginAPI {
     private val client = OkHttpClient()
 
-    public fun getGeneralSuggestion(callback: (input : MutableList<RecipeCard>) -> Unit) {
-        val request = Request.Builder().url("http://52.186.139.166/Suggestions/GetGeneralSuggestion" /*(Base_URL + ApiSection + "GetGeneralSuggestion")*/).build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                println(e.message)
-                println(e.stackTrace)
-            }
-            override fun onResponse(call: Call, response: Response) {
-                val body:ResponseBody? = response.body
-                callback((Json.decodeFromString<List<RecipeCard>>(body!!.string())).toMutableList());
-
-            }
-        })
-    }
-
-    public fun GetUsersRecipesByUser(callback: (input : MutableList<RecipeCard>) -> Unit) {
-            var JSON = "application/json; charset=utf-8".toMediaType()
-        var body:RequestBody = RequestBody.create(JSON, "{\"email\": \"adriangonzalezmadruga@gmail.com\"}");
+    public fun VerifyLogin(content: UserLogin, callback: (input : UserLogin) -> Unit) {
+        var JSON = "application/json; charset=utf-8".toMediaType()
+        var body:RequestBody = RequestBody.create(JSON, "{\"email\": \""+content.Email+"\", \"password\": \""+content.Password+"\", \"result\": \""+content.Result+"\",}");
         /*val formBody: RequestBody = FormBody.Builder()
             //.add("email", "adriangonzalezmadruga@gmail.com")
             .build()*/
         val request: Request = Request.Builder()
-            .url("http://52.186.139.166/Suggestions/GetUsersRecipesById")
+            .url("http://52.186.139.166/Login/CheckLogin")
             .post(body)
             .build()
 
@@ -53,7 +42,7 @@ class SuggestionsAPI {
             }
             override fun onResponse(call: Call, response: Response) {
                 val body:ResponseBody? = response.body
-                callback((Json.decodeFromString<List<RecipeCard>>(body!!.string())).toMutableList());
+                callback((Json.decodeFromString<UserLogin>(body!!.string())).toMutableList());
 
             }
         })
---
+++ app/src/main/java/com/jam/recipeassistant/model/Login/UserLogin.kt
@@ -0,0 +1,11 @@
+package com.jam.recipeassistant.model.Login
+
+/**
+ * Created by 991470628 : MARCO HIDALGO ROMERO
+ * on 2022-01-30
+ */
+data class UserLogin(
+    var Email : String,
+    var Password : String,
+    var Result: Int
+)
