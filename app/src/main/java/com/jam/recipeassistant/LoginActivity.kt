71cbcb822e47800a26cebbb58a1e04e2413addcf
Marco Hidalgo-Romero <marcohcanada@gmail.com>
2022-02-03 09:43:14 -0500

    Made notification clickable

--- app/src/main/java/com/jam/recipeassistant/LoginActivity.kt
+++ app/src/main/java/com/jam/recipeassistant/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -76,10 +77,21 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun sendNotification() {
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val bitmap = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.send_icon)
+        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.send)
+
         val builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Example Title")
             .setContentText("Example Description")
+            .setLargeIcon(bitmapLargeIcon)
+            .setStyle(NotificationCompat.BigTextStyle().bigText("Much longer text that cannot fit one line so we need to extend it to be much, much longer"))
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         with(NotificationManagerCompat.from(this)) {
