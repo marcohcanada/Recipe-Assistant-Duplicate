e9133df0d933a3646e78522dc318ff45afd9eef7
gnanaJoel <43079208+gnanaJoel@users.noreply.github.com>
2022-02-04 21:20:07 -0500

    Created and initially implemented the Setting Fragment Kotlin class and its associated XML layout file and navigation destination and actions in the Layout folder and Navigation folder, respectively in the Resources folder; Updated the the Settings Fragment kotlin class and its associated XML layout file by initially integrating data binding functionality in the Layout folder of the resources folder as well as in the App folder, respectively, in the Recipe Assistant Mobile Application Android Studio Project.

---
+++ app/src/main/java/com/jam/recipeassistant/SettingsFragment.kt
@@ -0,0 +1,21 @@
+package com.jam.recipeassistant
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jam.recipeassistant.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
